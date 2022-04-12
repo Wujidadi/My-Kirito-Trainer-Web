fc0ebc56910d6579a4e8a2d6ae09621db5df28fe
Wujidadi <wujidadi@gmail.com>
2022-04-12 09:54:28 +0800

    所有頁面改用 PHP heredoc 構成

--- view-player-log.php
+++ view-player-log.php
@@ -1,37 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>列出玩家日誌</title>
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-        body {
-            margin: 0;
-        }
-        pre#command-output {
-            background-color: lavender;
-            color: mediumblue;
-            margin: 8px;
-            padding: 5px;
-            border-radius: 5px;
-            min-height: 25px;
-            white-space: pre-wrap;
-            word-break: break-all;
-        }
-        div#button-area {
-            height: 36px;
-            margin: 8px;
-            padding-top: 5px;
-        }
-        button {
-            width: 100px;
-            height: 25px;
-        }
-    </style>
 <?php
 
 require_once 'storage/token.php';
@@ -40,45 +6,69 @@ if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || $_SE
 {
     header('www-authenticate: Basic realm="Wujidadi\'s MyKirito Trainer"');
     header($_SERVER['SERVER_PROTOCOL'] . ' 401 Unauthorized');
-
-?>
-</head>
-<body>
-</body>
-</html><?php
-
     exit;
 }
 else
 {
     require_once 'lib/helpers.php';
 
-?>
-</head>
-<body>
-    <pre id="command-output"><?php
+    $player = $_GET['p'];
+    $logType = $_GET['t'];
+    $logLevel = $_GET['v'];
+    $logDate = $_GET['d'];
+    $logLine = $_GET['l'];
+    $output = htmlentities(recoverCliOutput(shell_exec("export LANG=C.UTF-8; php /home/wujidadi/MyKiritoCommands/ViewPlayerLog --player={$player} --type={$logType} --level={$logLevel} --date={$logDate} --line={$logLine}")));
 
-$player = $_GET['p'];
-$logType = $_GET['t'];
-$logLevel = $_GET['v'];
-$logDate = $_GET['d'];
-$logLine = $_GET['l'];
-$output = recoverCliOutput(shell_exec("export LANG=C.UTF-8; php /home/wujidadi/MyKiritoCommands/ViewPlayerLog --player={$player} --type={$logType} --level={$logLevel} --date={$logDate} --line={$logLine}"));
-echo htmlentities($output);
+    $page = <<<HTML
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>列出玩家日誌</title>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+            body {
+                margin: 0;
+            }
+            pre#command-output {
+                background-color: lavender;
+                color: mediumblue;
+                margin: 8px;
+                padding: 5px;
+                border-radius: 5px;
+                min-height: 25px;
+                white-space: pre-wrap;
+                word-break: break-all;
+            }
+            div#button-area {
+                height: 36px;
+                margin: 8px;
+                padding-top: 5px;
+            }
+            button {
+                width: 100px;
+                height: 25px;
+            }
+        </style>
+    </head>
+    <body>
+        <pre id="command-output">{$output}</pre>
+        <div id="button-area">
+            <button id="back">Back</button>
+        </div>
+        <script>
+            const btnBack = document.querySelector('#back');
+            btnBack.addEventListener('click', function() {
+                location.href = '/';
+            });
+        </script>
+    </body>
+    </html>
+    HTML;
 
-?></pre>
-    <div id="button-area">
-        <button id="back">Back</button>
-    </div>
-    <script>
-        const btnBack = document.querySelector('#back');
-        btnBack.addEventListener('click', function() {
-            location.href = '/';
-        });
-    </script>
-</body>
-</html><?php
-
+    echo $page;
 }
-
-?>
