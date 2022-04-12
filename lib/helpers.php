39b586fece0df31621f1e17627000e47338df96f
Wujidadi <wujidadi@gmail.com>
2022-04-12 09:52:45 +0800

    更新 recoverCliOutput 輔助函數

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,14 +1,25 @@
 <?php
 
+/**
+ * 移除命令行輸出內容中的 ANSI 顏色控制碼及前後空行
+ *
+ * @param  string  $text  命令行輸出內容
+ * @return string|false
+ */
 function recoverCliOutput($text)
 {
-    return preg_replace(
-        [
-            "/^\n+/",
-            "/\033\[38;2;\d{1,3};\d{1,3};\d{1,3}m/",
-            "/\033\[0m/"
-        ],
-        '',
-        $text
-    );
+    if (is_string($text))
+    {
+        return preg_replace(
+            [
+                "/^\n+/",
+                "/\033\[38;2;\d{1,3};\d{1,3};\d{1,3}m/",
+                "/\033\[0m/",
+                "/\n+$/"
+            ],
+            '',
+            $text
+        );
+    }
+    return false;
 }
