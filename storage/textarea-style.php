9391a833118b5ab2568f0f04d3cbe131a1ace03b
Wujidadi <wujidadi@gmail.com>
2022-05-14 18:05:35 +0800

    補上 `textarea-style` 暗色模式

--- storage/textarea-style.php
+++ storage/textarea-style.php
@@ -10,11 +10,18 @@ $style = <<<CSS
 }
 body {
     margin: 0;
+    background-color: #222;
+    color: #ddd;
 }
 div#conf {
     margin: 8px auto auto auto;
     width: calc(100vw - 16px);
     height: 80vh;
+}
+button, input, textarea, select {
+    background-color: #363636;
+    color: #ddd;
+    border-style: none;
 }
 textarea {
     width: 100%;
@@ -32,6 +39,21 @@ button {
 }
 button:hover {
     background-color: #424242;
+}
+::-webkit-scrollbar {
+    width: 13px;
+    height: 13px;
+    background-color: #303030;
+}
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    border-radius: 4px;
+    background-color: #303030;
+}
+::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    background-color: #555;
 }
 CSS;
 $style = indentHTML($style, 4, 2, true);
